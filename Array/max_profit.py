ddee41e18b5fdf88c700936b44cc59a94eb3f316
Ketaki Ghatole <52592007+98ketaki@users.noreply.github.com>
2022-07-22 00:12:33 -0400

    Update max_profit.py

--- Array/max_profit.py
+++ Array/max_profit.py
@@ -1,10 +1,13 @@
-  #https://leetcode.com/problems/best-time-to-buy-and-sell-stock/submissions/
+#https://leetcode.com/problems/best-time-to-buy-and-sell-stock/
   
-  def maxProfit(self, prices):
-    max1=0
-    min1=float('inf')
-    for p in prices:
-      min1=min(min1, p)
-      profit=p-min1
-      max1=max(max1, profit)
-    return max1
+def maxProfit(self, prices):
+  max1=0
+  min1=float('inf')
+  for p in prices:
+    min1=min(min1, p)
+    profit=p-min1
+    max1=max(max1, profit)
+  return max1
+
+#Sample
+print(maxProfit([7,1,5,3,6,4]))
